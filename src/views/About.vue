70ccc80ffda3c9899407af91b21852841ed8ca15
fazanata <faz0@ya.ru>
2021-03-12 11:56:41 +0300

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>тестовое задание. Тестирование</h1>
+    <img src="https://1.bp.blogspot.com/-GY6IOgrChlU/W4z7XbgUiII/AAAAAAAABkU/VgbPn3_Z5sEjyMssczXRq9ARF-WbgZ75wCLcBGAs/s640/Depositphotos_68972119_l-2015.jpg"/>
+  </div>
+</template>
