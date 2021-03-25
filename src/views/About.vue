2e794a8def67c5c54e479f2d23bb1575fa4e76f6
fazanata <faz0@ya.ru>
2021-03-25 22:34:46 +0300

    fix post

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,11 +4,17 @@
     Vue js 2, Webpack 4, php, mysql
     <p>Vue Router</p><br />
     <br />
-    http://test.ce74911.tmweb.ru/api_test/result/readResults.php -чтение результатов
+    http://test.ce74911.tmweb.ru/api_test/result/create.php - запись результата теста<br />
+    {<br />
+        result: array,<br />
+        test_id: 1,<br />
+        user_id: 1<br />
+    <br /><br />
+    http://test.ce74911.tmweb.ru/api_test/result/readResults.php?idt=1 -чтение результатов теста 1
     <br />
     http://test.ce74911.tmweb.ru/api_test/category/readCategories.php - чтение списка категорий
     <br />
-    http://test.ce74911.tmweb.ru/api_test/category/readCategory.php?idc=3  - чтение данных категории 
+    http://test.ce74911.tmweb.ru/api_test/category/readCategory.php?idc=3  - чтение данных категории 3
   <br />
   ERD:<br /><br />
   <img class="gallery__img" src="../assets/erd.jpg" alt="erd" />
